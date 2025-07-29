96f7c42ea6ea77b727ecee4a93c607538c790b27
markselvan <75470574+markselvan@users.noreply.github.com>
2025-07-29 16:48:44 +0800

    added new files for learning

--- hello.py
+++ hello.py
@@ -1,26 +1,11 @@
-def is_adult(age):
-    if age >= 18:
-        print('adult')
-    else:
-        print('minor')
-is_adult(20)  # Call the function to see output
+class car:
+    def __init__(self, brand, color);
+        self.brand = brand
+        self.color = color
 
-fruits = ["apples", "banana", "cheries"]
-print(fruits[0])
+    def drive(self):
+        print(f"The {self.brand} {self.color} is driving.")
 
-print(help(fruits))
-print(len(fruits))
 
-fruits.append("pineapple")
-fruits.sort()
-
-fruits.count("banana")
-
-for x in fruits: #fruits is the list and fruit is a variable that takes each value in the list one by one
-    print(x)
-
-total = 0
-for i in range(1, 6):
-    total += i  # same as total = total + i
-
-print(total)  # Output: 15
+my_car = car("toyota", "red")
+my_car.drive()
